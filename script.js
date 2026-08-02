562dedb2dc89bf0141e6fefba6b93dc1d5af69d1
V1D30 <55475689+V1D30@users.noreply.github.com>
2026-08-02 16:49:36 +1000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".mobile-nav");
-
-menu?.querySelectorAll("a").forEach((link) => {
-  link.addEventListener("click", () => menu.removeAttribute("open"));
-});
-
-const year = document.querySelector("#year");
-if (year) year.textContent = new Date().getFullYear();
